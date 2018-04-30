706ec5a9d97ee3636689e01fed6c587ac86352da
adzbritton <adzbritton@btinternet.com>
2018-04-30 19:52:22 +0100

    adding new portfolio

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-var css = document.querySelector ("h3");
-var color1 = document.querySelector (".color1");
-var color2 = document.querySelector (".color2");
-var body = document.getElementById ("gradient");
+const css = document.querySelector ("h3");
+const color1 = document.querySelector (".color1");
+const color2 = document.querySelector (".color2");
+const color3 = document.querySelector ("color3");
+const body = document.getElementById ("gradient");
+const colorChange = document.querySelector("colorChange");
 
-function setGradient() {
-	body.style.background = 
+const setGradient() => 	body.style.background = 
 		"linear-gradient(to right, "
 		+ color1.value
 		+ ","
 		+ color2.value
 		+ ")";
 	css.textContent = body.style.background + ";";
-}
+
+const textGradient () =>
+	colorChange.style.color = color3.value;
 
 color1.addEventListener ("input", setGradient);
 color2.addEventListener ("input", setGradient);
+color3.addEventListener ("input", textGradient);
